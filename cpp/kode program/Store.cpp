70aa8ce65d5b2637ce9eb43f5d3795e97611db77
naurafaizah <naurafaizah@upi.edu>
2025-09-24 19:27:15 +0700

    Update Store.cpp

--- cpp/kode program/Store.cpp
+++ cpp/kode program/Store.cpp
@@ -2,17 +2,21 @@
 #include "Electronic.cpp"
 using namespace std;
 
+// class store merupakan turunan dari electronic
+// menambahkan atribut khusus seperti lokasi, nama penjual, dan kontak
 class Store : public Electronic{
     private:
-        string location;
-        string sellerName;
-        string contact;
+        string location;    // menyimpan lokasi toko atau produk
+        string sellerName;  // menyimpan nama penjual
+        string contact;     // menyimpan kontak penjual
 
     public:
+    // constructor kosong
     Store() {
 
     }
 
+    // constructor dengan parameter, mewarisi atribut dari electronic
     Store(int id, string name, int price, int stock, string image,
                 string brand, string warranty, string category,
                 string location, string sellerName, string contact) : Electronic(id, name, price, stock, image, brand, warranty, category){
